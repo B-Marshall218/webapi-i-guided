af8ef3008e818df4fd8f6d4bb5162a566f5dd29a
Brian <lawmen218@yahoo.com>
2019-11-02 21:50:46 -0400

    created a sever file

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const db = require("./data/hubs-model.js")
+
+const server = express();
+
+server.listen(4000, () => {
+    console.log("=== server listening on port 4000 ===");
+
+});
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send("hello world from Express");
+})
+
+//CRUD: Create Read Update Delete 
+
+//Read for hubs
+server.get("/hubs", (req, res) => {
+    db.find()
+        .then(hubs => {
+            res.status(200).json(hubs);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: err,
+                success: false
+            });
+        });
+
+});
+// C
+server.post("/hubs", (req, res) => {
+    const hubInfo = req.body;
+
+    db.add(hubInfo)
+        .then((hub) => {
+            res.status(201).json({ sucess: true, hub })
+                .catch((err) => {
+                    res.status(500).json({ success: false, err })
+                })
+        })
+})
+
+server.delete("/hubs/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.remove(id)
+        .then(deletedHub => {
+            if (deletedHub) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ message: `i could not find id = ${id}` });
+            }
+        })
+
+        .catch(err => {
+            res.status(500).json({ success: false, err });
+        })
+});
+
+server.put("/hubs/:id", (req, res) => {
+    const id = req.params.id;
+    const hubInfo = req.body;
+
+    db.update(id, changes)
+        .then(hub => {
+            if (hub) {
+                res.status(200).json({ success: true, hub });
+            } else {
+                res.status(200).json({ success: false, message: `id ${id} does not exist ` })
+            }
+        })
+})
+
+
